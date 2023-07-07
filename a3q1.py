25b029244a137633994a9938f3acc928dafef3e0
ALinda683 <ugj683@usask.ca>
2023-07-07 12:46:17 -0600

    a3q1- A new function named main is created

--- a3q1.py
+++ a3q1.py
@@ -1,4 +1,15 @@
 # Name: ALinda Kumar Mazumder
 # NSID: ugj683
 # Student no: 11342454
+# CMPT-145
+import sys
+from Stack import Stack
+from Queue import Queue
+
+
+def main():
+    if len(sys.argv) != 2 or sys.argv[1] not in ['s', 'q']:
+        print("Usage: python3 a3q1.py <container_type>")
+        print("Container types: 's' for stack, 'q' for queue")
+        return
 
